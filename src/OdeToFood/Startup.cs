c7cc6c651d43fd04e786f9474be3ab214fab73b3
Satish Thakur <sthakur@cecity.com>
2017-01-11 11:28:35 -0500

    test

--- src/OdeToFood/Startup.cs
+++ src/OdeToFood/Startup.cs
@@ -22,8 +22,11 @@ namespace OdeToFood
         // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
+
             services.AddSingleton(provider => Configuration);
             services.AddSingleton<IGreeter, Greeter>();
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -33,6 +36,7 @@ namespace OdeToFood
         {
             app.UseIISPlatformHandler();
 
+            
             app.Run(async (context) =>
             {
                 var greeting = greeter.GetGreeting();
